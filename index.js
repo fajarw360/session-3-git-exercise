35df2c0808d49681a927ac179028b311742d58ae
fajar wahyu <fajarw360@gmail.com>
2020-12-02 21:59:57 +0800

    Selamat Pagi

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = 3000
 const app = express()
 
 app.get('/', (req,res) => {
-    res.send('Hello World');
+    res.send('Selamat Pagi);
 });
 
 app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
